78b950c005bdbd34f817c7ef11a829d7cfdfa9ac
smf <1191840939@qq.com>
2020-04-20 22:17:23 +0800

    '4-20-1'

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,25 +1,42 @@
 <template>
   <div id="detail">
-
+    <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :top-images="topImages"></detail-swiper>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
+import DetailNavBar from "./childComps/DetailNavBar";
+import DetailSwiper from "./childComps/DetailSwiper";
 
+import {getDetail,Goods} from "network/detail";
 
-  export default {
+export default {
     name: "Detail",
     components:{
-      NavBar
+      DetailNavBar,
+      DetailSwiper
     },
     data(){
       return{
-        iid:null
+        iid:null,
+        topImages:[],
+        goods:null
       }
     },
     created() {
+     //1.保存传入的iid
       this.iid = this.$route.params.iid
+
+      //2.根据iid请求详情数据
+      getDetail(this.iid).then(res => {
+        //1.获取顶部的图片轮播数据
+        console.log(res);
+        this.topImages = res.result.itemInfo.topImages
+
+        //2.获取商品信息
+        this.goods= new Goods
+      })
     }
   }
 </script>
