3fe45e1b8a1d982eb7989fc15ef90d9f8eef019f
smf <1191840939@qq.com>
2020-04-21 17:17:29 +0800

    4.21-1

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,46 +1,115 @@
 <template>
-  <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
+  <div id="detail" >
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
+      <detail-param-info :param-info="paramInfo"></detail-param-info>
+      <detail-comment-info :comment-info="commentInfo"></detail-comment-info>
+      <goods-list :goods="getRecommends"/>
+    </scroll>
   </div>
 </template>
 
 <script>
-import DetailNavBar from "./childComps/DetailNavBar";
-import DetailSwiper from "./childComps/DetailSwiper";
+  import DetailNavBar from "./childComps/DetailNavBar";
+  import DetailSwiper from "./childComps/DetailSwiper";
+  import DetailBaseInfo from "./childComps/DetailBaseInfo"
+  import DetailShopInfo from "./childComps/DetailShopInfo"
+  import DetailGoodsInfo from "./childComps/DetailGoodsInfo"
+  import DetailParamInfo from  "./childComps/DetailParamInfo"
+  import DetailCommentInfo from  "./childComps/DetailCommentInfo"
 
-import {getDetail,Goods} from "network/detail";
 
-export default {
+  import Scroll from "components/common/scroll/Scroll";
+  import GoodsList from "components/content/goods/GoodsList"
+
+  import {getDetail, Goods,Shop,GoodsParam,getRecommend} from "network/detail";
+
+  export default {
     name: "Detail",
-    components:{
+    components: {
       DetailNavBar,
-      DetailSwiper
+      DetailSwiper,
+      DetailBaseInfo,
+      DetailShopInfo,
+      Scroll,
+      DetailGoodsInfo,
+      DetailParamInfo,
+      DetailCommentInfo,
+      GoodsList
     },
-    data(){
-      return{
-        iid:null,
-        topImages:[],
-        goods:null
+    data() {
+      return {
+        iid: null,
+        topImages: [],
+        goods: {},
+        shop:{},
+        detailInfo:{},
+        paramInfo:{},
+        commentInfo:{},
+        getRecommends:[]
       }
     },
+    methods:{
+      imageLoad(){
+        this.$refs.scroll.refresh()
+      }
+
+    },
     created() {
-     //1.保存传入的iid
+      //1.保存传入的iid
       this.iid = this.$route.params.iid
 
       //2.根据iid请求详情数据
       getDetail(this.iid).then(res => {
         //1.获取顶部的图片轮播数据
-        console.log(res);
-        this.topImages = res.result.itemInfo.topImages
+        // console.log(res);
+        const data = res.result
+        this.topImages = data.itemInfo.topImages
 
         //2.获取商品信息
-        this.goods= new Goods
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+        //3.创建店铺信息的对象
+        this.shop = new Shop(data.shopInfo)
+
+        //4.保存商品的详情数据
+        this.detailInfo = data.detailInfo
+
+        //5.获取参数信息
+        this.paramInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
+
+        //6.取出评论信息
+        if (data.rate.cRate !== 0){
+          this.commentInfo = data.rate.list[0]
+        }
+      })
+
+      //3.请求推荐数据
+      getRecommend().then(res=>{
+        this.getRecommends = res.data.list
       })
     }
   }
 </script>
 
 <style scoped>
+   #detail{
+     position: relative;
+     z-index: 9;
+     background-color: #fff;
+     height: 100vh;
+   }
+   .detail-nav{
+     position: relative;
+     z-index: 9;
+     background-color: #fff;
+   }
+  .content{
+    height: calc(100% - 44px);
+  }
 
 </style>
--- src/views/detail/childComps/DetailGoodsInfo.vue
+++ src/views/detail/childComps/DetailGoodsInfo.vue
@@ -29,6 +29,7 @@
     },
     methods: {
 	    imgLoad() {
+	      //判断 所有的图片加载完了，那么进行一次回调就可以了
         if (++this.counter === this.imagesLength) {
 	        this.$emit('imageLoad')
         }
